e3705dfab23a9db2723461bedf3545dd8f2dc2f6
Jorge Aparicio <japaricious@gmail.com>
2017-01-22 19:21:21 -0500

    fix syntax

--- ci/install.sh
+++ ci/install.sh
@@ -4,7 +4,7 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross | \
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
                     | cut -d/ -f3 \
                     | grep -E '^v0\.1\.[0-9]+$' \
                     | sort --version-sort \
